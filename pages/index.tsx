2af9f54f83a1c064f5d166611b5c608d25278b41
정주형 <juhyoungjung@cclss.net>
2023-01-10 16:05:05 +0900

    typo: All Posts => 더 보기

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,7 +88,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label="all posts"
           >
-            All Posts &rarr;
+            더 보기
           </Link>
         </div>
       )}
