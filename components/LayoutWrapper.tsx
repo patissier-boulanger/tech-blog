7e7af57771508ba1ae3bede63cc7bae95ddd009b
정주형 <juhyoungjung@cclss.net>
2023-01-10 17:48:47 +0900

    chore: add logo placeholder for navigate

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,3 +1,4 @@
+import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
@@ -14,7 +15,14 @@ const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-end py-10">
+        <header className="flex items-center justify-between py-10">
+          <div>
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="flex items-center justify-between">
+                <div className="mr-3 font-serif text-base font-black">Logo should be here</div>
+              </div>
+            </Link>
+          </div>
           <div className="flex items-center text-base leading-5">
             <div className="hidden sm:block">
               {headerNavLinks.map((link) => (
