fd6a194b2764f71d6c7ccfe33cc46630da07100c
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2023-01-18 23:42:42 +0900

    style: work on post simple layout

--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -26,7 +26,7 @@ export default function PostLayout({ frontMatter, next, prev, children }: Props)
       <article>
         <div>
           <header>
-            <div className="space-y-1 border-b border-gray-200 pb-10 text-center dark:border-gray-700">
+            <div className="space-y-1 border-b border-gray-200 pb-10 text-left dark:border-gray-700">
               <dl>
                 <div>
                   <dt className="sr-only">Published on</dt>
@@ -47,7 +47,7 @@ export default function PostLayout({ frontMatter, next, prev, children }: Props)
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
-            <Comments frontMatter={frontMatter} />
+            {/* <Comments frontMatter={frontMatter} /> */}
             <footer>
               <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
                 {prev && (
