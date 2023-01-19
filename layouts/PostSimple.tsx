47425115e41e8178af3bc920a1de29a98a230e1a
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2023-01-19 23:49:07 +0900

    chore: remove prev next article feature

--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -1,4 +1,3 @@
-import Link from '@/components/Link'
 import PageTitle from '@/components/PageTitle'
 import { BlogSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
@@ -16,7 +15,7 @@ interface Props {
   prev?: { slug: string; title: string }
 }
 
-export default function PostLayout({ frontMatter, next, prev, children }: Props) {
+export default function PostLayout({ frontMatter, children }: Props) {
   const { slug, date, title } = frontMatter
 
   return (
@@ -47,31 +46,6 @@ export default function PostLayout({ frontMatter, next, prev, children }: Props)
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
-            {/* <Comments frontMatter={frontMatter} /> */}
-            <footer>
-              <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
-                {prev && (
-                  <div className="pt-4 xl:pt-8">
-                    <Link
-                      href={`/blog/${prev.slug}`}
-                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      &larr; {prev.title}
-                    </Link>
-                  </div>
-                )}
-                {next && (
-                  <div className="pt-4 xl:pt-8">
-                    <Link
-                      href={`/blog/${next.slug}`}
-                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      {next.title} &rarr;
-                    </Link>
-                  </div>
-                )}
-              </div>
-            </footer>
           </div>
         </div>
       </article>
