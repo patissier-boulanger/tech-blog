83e4b30578e3e944c99e5ebfd2ecbe466760f46d
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2023-01-24 15:54:16 +0900

    style: rework footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,9 +9,7 @@ export default function Footer() {
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
         </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
+        <div className="mb-2 flex flex-col items-center justify-center space-x-2 text-sm text-gray-500 dark:text-gray-400 sm:flex-row">
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <div>Copyright by Juhyoung JUNG. All rights reserved.</div>
