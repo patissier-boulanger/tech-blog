8c5b7b7b842c2b293f070c08f6ff42edd5b3fb34
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2023-01-24 15:51:12 +0900

    feature: add # for tags

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -9,7 +9,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
       <a className="mr-3 text-sm font-medium uppercase text-primary-400 hover:text-primary-300 dark:hover:text-primary-400">
-        {text.split(' ').join('-')}
+        #{text.split(' ').join('-')}
       </a>
     </Link>
   )
