0fb87a0e88ddd0c2b5522829f1b83d7fac752c9c
정주형 <juhyoungjung@cclss.net>
2023-01-25 14:43:21 +0900

    style: change blog layout article space 4xl to 3xl

--- components/PostLayoutSectionContainer.tsx
+++ components/PostLayoutSectionContainer.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export default function PostLayoutSectionContainer({ children }: Props) {
-  return <div className="mx-auto my-10 max-w-3xl px-4 sm:px-6 xl:max-w-4xl xl:px-0">{children}</div>
+  return <div className="mx-auto my-10 max-w-3xl px-4 sm:px-6 xl:max-w-3xl xl:px-0">{children}</div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home({
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <IntroVideo />
       <div className="mt-7 flex items-center">
-        <h2 className="mr-2 pb-4 font-sans text-5xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h2 className="mr-2 pb-4 font-sans text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Articles
         </h2>
         <div className="h-0.5 flex-1 bg-gray-300"></div>
