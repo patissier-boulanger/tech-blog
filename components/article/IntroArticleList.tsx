558db32207a80f23d41869b24f49c2e6a0976e1c
정주형 <juhyoungjung@cclss.net>
2023-02-13 17:07:34 +0900

    style: add more height on intro article

--- components/article/IntroArticleList.tsx
+++ components/article/IntroArticleList.tsx
@@ -18,24 +18,24 @@ export function IntroArticleLists({ posts }: IntroArticleListsProp) {
             return (
               <li key={slug} className="flex h-full w-full flex-col py-12">
                 <article className="flex min-h-[14rem] flex-1 flex-col">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-sm font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>{formatDate(date)}</time>
-                    </dd>
-                  </dl>
-                  <div className="space-y-5">
-                    <div>
-                      <h2 className="min-h-[3rem] font-serif text-3xl font-bold leading-10 tracking-tight line-clamp-2">
-                        <Link href={`/blog/${slug}`} className="text-gray-800 dark:text-gray-100">
+                  <Link href={`/blog/${slug}`} className="text-gray-800 dark:text-gray-100">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-sm font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date)}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5">
+                      <div>
+                        <h2 className="min-h-[3rem] font-serif text-3xl font-bold leading-11 tracking-tight line-clamp-2 xl:min-h-[5.5rem]">
                           {title}
-                        </Link>
-                      </h2>
+                        </h2>
+                      </div>
+                      <div className="prose max-w-none font-serif text-gray-800 line-clamp-4 dark:text-gray-300 ">
+                        {summary}
+                      </div>
                     </div>
-                    <div className="prose max-w-none font-serif text-gray-800 line-clamp-4 dark:text-gray-300 ">
-                      {summary}
-                    </div>
-                  </div>
+                  </Link>
                 </article>
 
                 <div className="mt-12 flex flex-wrap">
