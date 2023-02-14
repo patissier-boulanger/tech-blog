73b8bb376c00a63d80925cb82474499dbeec9a27
정주형 <juhyoungjung@cclss.net>
2023-02-14 18:01:10 +0900

    fix: fix abusive intro video fetching

--- components/IntroVideo.tsx
+++ components/IntroVideo.tsx
@@ -14,20 +14,27 @@ export function IntroVideo() {
     <div className="min-h-[200px] flex-1">
       <div className="relative m-auto h-[350px] w-full sm:h-[600px] sm:w-[600px]">
         {mounted ? (
-          <video
-            className="absolute object-contain"
-            src={
-              theme === 'light'
-                ? '/static/videos/introVideo_Light.mp4'
-                : '/static/videos/introVideo_Dark.mp4'
-            }
-            autoPlay={true}
-            muted={true}
-            loop={true}
-            controls={false}
-            playsInline={true}
-            preload="auto"
-          ></video>
+          theme === 'light' ? (
+            <video
+              className="absolute object-contain"
+              src={'/static/videos/introVideo_Light.mp4'}
+              autoPlay={true}
+              muted={true}
+              loop={true}
+              controls={false}
+              playsInline={true}
+            ></video>
+          ) : (
+            <video
+              className="absolute object-contain"
+              src={'/static/videos/introVideo_Dark.mp4'}
+              autoPlay={true}
+              muted={true}
+              loop={true}
+              controls={false}
+              playsInline={true}
+            ></video>
+          )
         ) : (
           <></>
         )}
