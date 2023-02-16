da629f92b6e4e2983a47262514d700169272ae1c
정주형 <juhyoungjung@cclss.net>
2023-02-16 10:15:15 +0900

    fix: separte intro video

--- components/IntroVideo.tsx
+++ components/IntroVideo.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 export function IntroVideo() {
   const [mounted, setMounted] = useState(false)
 
-  const { theme } = useTheme()
+  const { resolvedTheme } = useTheme()
 
   useEffect(() => {
     setMounted(true)
@@ -14,19 +14,27 @@ export function IntroVideo() {
     <div className="min-h-[200px] flex-1">
       <div className="relative m-auto h-[350px] w-full sm:h-[600px] sm:w-[600px]">
         {mounted ? (
-          <video
-            className="absolute object-contain"
-            src={
-              theme === 'light'
-                ? '/static/videos/introVideo_Light.mp4'
-                : '/static/videos/introVideo_Dark.mp4'
-            }
-            autoPlay={true}
-            muted={true}
-            loop={true}
-            controls={false}
-            preload="auto"
-          ></video>
+          resolvedTheme === 'light' ? (
+            <video
+              className="absolute object-contain"
+              src={'/static/videos/introVideo_Light.mp4'}
+              autoPlay={true}
+              muted={true}
+              loop={true}
+              controls={false}
+              playsInline={true}
+            ></video>
+          ) : (
+            <video
+              className="absolute object-contain"
+              src={'/static/videos/introVideo_Dark.mp4'}
+              autoPlay={true}
+              muted={true}
+              loop={true}
+              controls={false}
+              playsInline={true}
+            ></video>
+          )
         ) : (
           <></>
         )}
